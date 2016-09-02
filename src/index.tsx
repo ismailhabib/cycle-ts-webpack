6059a5da306f4a08d0bdecb3c01a4d301c886ee4
Ismail Habib Muhammad <habsky@gmail.com>
2016-09-02 23:07:17 +0200

    Add the checkbox example

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,33 @@
-/// <reference path="../typings/index.d.ts" />
-
-import xs from 'xstream';
+import xs, {Stream} from 'xstream';
 import {run} from '@cycle/xstream-run';
-import {makeDOMDriver, h1} from '@cycle/dom';
+import {div, input, p, makeDOMDriver, VNode} from '@cycle/dom';
+import {DOMSource} from "@cycle/dom/xstream-typings";
 
-function main() {
+export type Sources = {
+    DOM: DOMSource,
+};
+export type Sinks = {
+    DOM: Stream<VNode>,
+}
+
+function main(sources:Sources): Sinks {
     const sinks = {
-        DOM: xs.periodic(1000).map(i =>
-            h1('' + i + ' seconds elapsed')
-        )
-    };
+        DOM: sources.DOM.select('input').events('change')
+            .map((ev: Event) => {
+                console.log(ev)
+                return ev.target.checked
+            })
+            .startWith(false)
+            .map((toggled:boolean) =>
+                div([
+                    input({attrs: {type: 'checkbox'}}), 'Toggle me',
+                    p(toggled ? 'ON' : 'off')
+                ])
+            )
+    }
     return sinks;
 }
 
-const drivers:any = {
+run(main, {
     DOM: makeDOMDriver('#app')
-};
-
-run(main, drivers);
+});
